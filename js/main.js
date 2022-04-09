83792e975ff53570df98397a75eeaa9f3b76a8f1
Jakub Sancho Gożuk <70905916+JSanchoG@users.noreply.github.com>
2022-04-09 18:35:34 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+date_future = new Date(new Date().getFullYear(), 3, 24, 19, 5);
+date_now = new Date();
+
+seconds = Math.floor((date_future - date_now) / 1000);
+minutes = Math.floor(seconds / 60);
+hours = Math.floor(minutes / 60);
+days = Math.floor(hours / 24);
+
+hours = hours - days * 24;
+minutes = minutes - days * 24 * 60 - hours * 60;
+seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+
+$("#d").text(`${days}`);
+$("#h").text(`${hours}`);
+$("#m").text(`${minutes}`);
+$("#s").text(`${seconds}`);
+$(function () {
+  var calcNewYear = setInterval(function () {
+    date_future = new Date(new Date().getFullYear(), 3, 24, 19, 5);
+    date_now = new Date();
+
+    seconds = Math.floor((date_future - date_now) / 1000);
+    minutes = Math.floor(seconds / 60);
+    hours = Math.floor(minutes / 60);
+    days = Math.floor(hours / 24);
+
+    hours = hours - days * 24;
+    minutes = minutes - days * 24 * 60 - hours * 60;
+    seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+
+    $("#d").text(`${days}`);
+    $("#h").text(`${hours}`);
+    $("#m").text(`${minutes}`);
+    $("#s").text(`${seconds}`);
+  }, 1000);
+});
