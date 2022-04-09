b2f0da5a7e95e2fb537848bfe508f3302ab32f86
Jakub Sancho Gożuk <70905916+JSanchoG@users.noreply.github.com>
2022-04-09 18:39:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,3 +34,26 @@ $(function () {
     $("#s").text(`${seconds}`);
   }, 1000);
 });
+
+$(".simpleslide100").each(function () {
+  var delay = 5000;
+  var speed = 1000;
+  var itemSlide = $(this).find(".simpleslide100-item");
+  var nowSlide = 0;
+
+  $(itemSlide).hide();
+  $(itemSlide[nowSlide]).show();
+  nowSlide++;
+  if (nowSlide >= itemSlide.length) {
+    nowSlide = 0;
+  }
+
+  setInterval(function () {
+    $(itemSlide).fadeOut(speed);
+    $(itemSlide[nowSlide]).fadeIn(speed);
+    nowSlide++;
+    if (nowSlide >= itemSlide.length) {
+      nowSlide = 0;
+    }
+  }, delay);
+});
