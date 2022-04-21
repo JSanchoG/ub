9e2b198548f1e5911234c7051c116d50420b1908
Jakub Sancho Gożuk <70905916+JSanchoG@users.noreply.github.com>
2022-04-21 18:18:48 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
-date_future = new Date(new Date().getFullYear(), 3, 24, 19, 4, 12);
-date_now = new Date();
+start();
+function start() {
+  date_future = new Date(2022, 3, 24, 19, 4, 12);
+  date_now = new Date();
 
-seconds = Math.floor((date_future - date_now) / 1000);
-minutes = Math.floor(seconds / 60);
-hours = Math.floor(minutes / 60);
-days = Math.floor(hours / 24);
+  seconds = Math.floor((date_future - date_now) / 1000);
+  minutes = Math.floor(seconds / 60);
+  hours = Math.floor(minutes / 60);
+  days = Math.floor(hours / 24);
 
-hours = hours - days * 24;
-minutes = minutes - days * 24 * 60 - hours * 60;
-seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+  hours = hours - days * 24;
+  minutes = minutes - days * 24 * 60 - hours * 60;
+  seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+  if (days < 0) {
+    $("#d").text("Unbanned!");
+    $(".tohaft").hide();
+    // $.getScript("js/fireworks.js");
+    // $("#c").show();
+    return;
+  }
 
-$("#d").text(`${days}`);
-$("#h").text(`${hours}`);
-$("#m").text(`${minutes}`);
-$("#s").text(`${seconds}`);
+  $("#d").text(`${days}`);
+  $("#h").text(`${hours}`);
+  $("#m").text(`${minutes}`);
+  $("#s").text(`${seconds}`);
+}
+
 $(function () {
   var calcNewYear = setInterval(function () {
-    date_future = new Date(new Date().getFullYear(), 3, 24, 19, 4, 12);
+    date_future = new Date(2022, 3, 24, 19, 4, 12);
     date_now = new Date();
-
     seconds = Math.floor((date_future - date_now) / 1000);
     minutes = Math.floor(seconds / 60);
     hours = Math.floor(minutes / 60);
@@ -28,6 +38,15 @@ $(function () {
     minutes = minutes - days * 24 * 60 - hours * 60;
     seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
 
+    if (days < 0) {
+      $("#d").text("Unbanned!");
+      $(".tohaft").hide();
+      clearInterval(calcNewYear);
+      $.getScript("js/fireworks.js");
+      // $("#c").show();
+
+      return;
+    }
     $("#d").text(`${days}`);
     $("#h").text(`${hours}`);
     $("#m").text(`${minutes}`);
